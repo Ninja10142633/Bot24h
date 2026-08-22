c9d2f1f51551274aab83607d223744282a2ef83e
Ninja10142633 <ninja1122551ttttyyjrh@gmail.com>
2026-08-22 16:02:40 -0300

    fix: update stub with Client and Server classes

--- raknet-native-stub/index.js
+++ raknet-native-stub/index.js
@@ -1,3 +1,27 @@
 // Stub module - replaces raknet-native to avoid native binary crashes
-// bedrock-protocol will fall back to jsp-raknet (JS backend)
-module.exports = {};
+// bedrock-protocol will use jsp-raknet (JS backend) instead
+
+class Client {
+  constructor() {}
+  connect() {}
+  close() {}
+  send() {}
+  ping() {}
+  on() { return this; }
+  once() { return this; }
+  removeListener() { return this; }
+  removeAllListeners() { return this; }
+}
+
+class Server {
+  constructor() {}
+  listen() {}
+  close() {}
+  send() {}
+  on() { return this; }
+  once() { return this; }
+  removeListener() { return this; }
+  removeAllListeners() { return this; }
+}
+
+module.exports = { Client, Server };
